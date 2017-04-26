fb3e6adc9d772cae138213f2e3c2f746514149ae
adish450 <adishirfan17@gmail.com>
2017-04-27 00:13:53 +0530

    Add files via upload

---
+++ lfr_2s.ino
@@ -0,0 +1,51 @@
+/*------ Program for Line Follower Robot using Arduino----- */
+/*-------definning Inputs------*/
+#define LS A1      // left sensor
+#define RS A4      // right sensor
+
+/*-------definning Outputs------*/
+#define LM1 13       // left motor
+#define LM2 12      // left motor
+#define RM1 9       // right motor
+#define RM2 8       // right motor
+
+void setup()
+{
+  pinMode(LS, INPUT);
+  pinMode(RS, INPUT);
+  pinMode(LM1, OUTPUT);
+  pinMode(LM2, OUTPUT);
+  pinMode(RM1, OUTPUT);
+  pinMode(RM2, OUTPUT);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+}
+
+void loop()
+{
+  if(!digitalRead(LS) && !digitalRead(RS))     // Move Forward
+  {
+    analogWrite(LM1, 120);
+    analogWrite(RM1, 120);
+  }
+  
+  if(digitalRead(LS) && !digitalRead(RS))     // Turn right
+  {
+    analogWrite(LM1, 0);
+    a
+    
+    nalogWrite(RM1, 120);
+  }
+  
+  if(!digitalRead(LS) && digitalRead(RS))     // turn left
+  {
+    analogWrite(LM1, 120);
+    analogWrite(RM1, 0);
+  }
+  
+  if(digitalRead(LS) && digitalRead(RS))     // stop
+  {
+     analogWrite(LM1, 120);
+    analogWrite(RM1, 120);
+  }
+}
